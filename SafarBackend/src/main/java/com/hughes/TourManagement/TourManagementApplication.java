39f2ebabe14e14aed1226851471fd06f08546835
Spydy-coder <bikashempire321@gmail.com>
2024-04-11 18:28:17 +0530

    cors policy update

--- SafarBackend/src/main/java/com/hughes/TourManagement/TourManagementApplication.java
+++ SafarBackend/src/main/java/com/hughes/TourManagement/TourManagementApplication.java
@@ -2,14 +2,28 @@ package com.hughes.TourManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class TourManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TourManagementApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+
+			}
+		};
 	}
 
 }
