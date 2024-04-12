b72a49f0225466fad3ad6292138822b6efd3cbd3
Spydy-coder <bikashempire321@gmail.com>
2024-04-12 21:38:46 +0530

    reviews structure

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -7,41 +7,37 @@ import ReviewCard from "./ReviewCard";
 export default function Reviews() {
   return (
     <div className="bg container " id="reviews">
-      <h4 className="text-uppercase first-heading ">Where Adventures Begin:</h4>
-      <h2 className="brand-name  ms-2 ">Safar</h2>
-      <h2 className="second-heading ">Customer Reviews</h2>
-      <div className="row mt-4">
-        <div className="col-12 col-sm-6">
-          <div className="row ">
-            <div className="col">
-              <ReviewCard
-                username={"Bikash Dalai"}
-                destination={"Goa Trip"}
-                text={`Safar exceeded all expectations! Their exceptional attention to detail made our trip unforgettable. `}
-              />
-            </div>
-            <div className="col"></div>
+      <div className="container-header">
+        <h4 className="text-uppercase first-heading ">
+          Where Adventures Begin:
+        </h4>
+        <h2 className="brand-name  ms-2 ">Safar</h2>
+        <h2 className="second-heading ">Customer Reviews</h2>
+      </div>
+
+      <div className="row mt-4 container-body">
+        <div className="col-12 col-sm-6" style={{ height: "100%" }}>
+          <div className="row first-card p-2 d-flex justify-content-center align-items-center" style={{ height: "31%" }}>
+            <ReviewCard
+              username={"Bikash Dalai"}
+              destination={"Goa Trip"}
+              text={`Safar exceeded all expectations! Their exceptional attention to detail made our trip unforgettable. `}
+            />
           </div>
-          <div className="row">
-            <div className="col"></div>
-            <div className="col">
-              <ReviewCard
-                username={"Anshuman Saini"}
-                destination={"Sikkim Trip"}
-                text={`Safar made our trip unforgettable! Exceptional service and
+          <div className="row p-2 d-flex justify-content-center align-items-center" style={{ height: "31%" }}>
+            <ReviewCard
+              username={"Anshuman Saini"}
+              destination={"Sikkim Trip"}
+              text={`Safar made our trip unforgettable! Exceptional service and
             breathtaking experiences. Highly recommend!`}
-              />
-            </div>
+            />
           </div>
-          <div className="row">
-            <div className="col">
-              <ReviewCard
-                username={"Aakash"}
-                destination={"Manali Trip"}
-                text={`Our trip was amazing! The experiences they curated for us were truly breathtaking.`}
-              />
-            </div>
-            <div className="col"></div>
+          <div className="row p-2 d-flex justify-content-center align-items-center" style={{ height: "31%" }}>
+            <ReviewCard
+              username={"Aakash"}
+              destination={"Manali Trip"}
+              text={`Our trip was amazing! The experiences they curated for us were truly breathtaking.`}
+            />
           </div>
         </div>
       </div>
